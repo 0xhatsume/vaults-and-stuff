6066777b38bb8862c726145558344012f8a84ffb
0xhatsume <0xhatsume@gmai.com>
2022-09-04 19:30:36 +0800

    feat: convert walletbutton to dynamic network connect

--- src/components/WalletButton/WalletButton.tsx
+++ src/components/WalletButton/WalletButton.tsx
@@ -1,14 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { useAccount, useConnect, useDisconnect, useNetwork } from "wagmi";
+import { shortenAddress } from '../../utils';
 
 export const WalletButton = () => {
+    const { address, connector: activeConnector, 
+        isConnecting, isConnected} = useAccount();
+    const { connect, connectors, data } = useConnect();
+    const {chain} = useNetwork();
+    const { disconnect } = useDisconnect();
+    
     return (
-        <button type="button" className=
+
+        isConnecting?
+        (
+            <button type="button" className=
+                'text-white bg-blue-300 \
+                    font-medium rounded-lg text-sm px-5 py-2.5 \
+                    text-center mr-3 md:mr-0 \
+                    dark:bg-blue-200'>
+                Connecting...
+                </button>
+        )
+        :
+        (
+            isConnected?
+            (
+                <button type="button" className=
+                    "text-white bg-green-500 hover:bg-green-800 \
+                        focus:ring-4 focus:outline-none focus:ring-green-300 \
+                        font-medium rounded-lg text-sm px-5 py-2.5 \
+                        text-center mr-3 md:mr-0 \
+                        dark:bg-green-400 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                        
+                        onClick={()=>{
+                            console.log('disconnecting...')
+                            disconnect()
+                        }}
+                        >
+                        {shortenAddress(address as string , 4)}
+                    </button>
+            ):
+            (<button type="button" className=
                 "text-white bg-blue-700 hover:bg-blue-800 \
                     focus:ring-4 focus:outline-none focus:ring-blue-300 \
                     font-medium rounded-lg text-sm px-5 py-2.5 \
                     text-center mr-3 md:mr-0 \
-                    dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    
+                    onClick={()=>{
+                        console.log("connect clicked.")
+                        connect({connector: connectors[0]})
+                    }}
+                    >
                 Connect Wallet
                 </button>
+            )
+        )
     )
 }
