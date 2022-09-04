74d9fe8dbc84867985272204bb92863bed231fc4
0xhatsume <0xhatsume@gmai.com>
2022-09-04 18:23:26 +0800

    ci: update build params for github actions build for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
+  assetPrefix: isProd ? '/vaults-and-stuff/' : '',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 }
