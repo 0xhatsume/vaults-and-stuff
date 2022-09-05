61e46f79c6651460dd66572bccf32d168af23d2e
0xhatsume <0xhatsume@gmai.com>
2022-09-05 13:03:26 +0800

    feat: update with redux providers

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export * from './BasePage';
 export * from './VaultCard';
 export * from './NavBar';
 export * from './WalletButton';
+export * from './Modals';
+export * from './Updaters';
+export * from './Overlay';
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,19 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { WagmiConfig } from 'wagmi';
 import { wagmiclient } from '../utils';
+import { Provider } from "react-redux";
+import store from "../redux/store";
+import {Modals, Updaters} from "../components";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiclient}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+        <Modals/>
+        <Updaters/>
+      </Provider>
     </WagmiConfig>
     )
 }
