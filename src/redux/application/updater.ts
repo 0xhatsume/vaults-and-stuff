4f6117e8cb24dbbdc383525a5e4681044a37a3be
0xhatsume <0xhatsume@gmai.com>
2022-09-05 13:05:42 +0800

    fix: remove error imports for updater

--- src/redux/application/updater.ts
+++ src/redux/application/updater.ts
@@ -1,7 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
 import { useIsWindowVisible } from "../../hooks";
-import { updateBlockNumber } from ".";
-import { useDispatch } from "react-redux";
 
 // currently placeholder for other updaters
 export default function Updater(): null {
